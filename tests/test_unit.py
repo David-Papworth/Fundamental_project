1b4413917cd175831d0744a640a5447b66f7bddb
Pappy <dpapworth@academytrainee.com>
2021-05-12 09:06:21 +0000

    unit tests complete

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -8,7 +8,7 @@ from application.models import Army, Figure
 class TestBase(TestCase):
     def create_app(self):
         app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///test.db",
-        SECRET_KEY='TEST_SECRET_KEY',
+        SECRET_KEY='SECRET_KEY',
         WTF_CSRF_ENABLED=False,
         DEBUG=True,
         )
@@ -31,15 +31,15 @@ class TestViews(TestBase):
         response = self.client.get(url_for('home'))
         self.assertEqual(response.status_code, 200)
     
-    def test_add_army_get(self):
+    def test_add_figure_get(self):
         response = self.client.get(url_for('add_figure'))
         self.assertEqual(response.status_code, 200)
 
-    def test_update_army_get(self):
+    def test_update_figure_get(self):
         response = self.client.get(url_for('update_figure', id=1), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
-    def test_delete_army_get(self):
+    def test_delete_figure_get(self):
         response = self.client.get(url_for('delete_figure', id=1), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
@@ -63,6 +63,7 @@ class TestRead(TestBase):
     def test_read_army(self):
         response = self.client.get(url_for('view_army'))
         self.assertIn(b"This is my main army", response.data)
+        self.assertIn(b"Army 1", response.data)
 
 class TestCreate(TestBase):
     def test_create_army(self):
@@ -71,6 +72,7 @@ class TestCreate(TestBase):
         follow_redirects=True
         )
         self.assertIn(b"This is my new army", response.data)
+        self.assertIn(b"new army", response.data)
 
 class TestUpdate(TestBase):
     def test_update_army(self):
@@ -79,6 +81,7 @@ class TestUpdate(TestBase):
         follow_redirects=True
         )
         self.assertIn(b"This is a updated a army", response.data)
+        self.assertIn(b"Update army", response.data)
 
 class TestDelete(TestBase):
     def test_delete_army(self):
@@ -86,3 +89,44 @@ class TestDelete(TestBase):
         follow_redirects=True
         )
         self.assertNotIn(b"This is my main army", response.data)
+        self.assertNotIn(b"Army 1", response.data)
+
+class TestRead1(TestBase):
+    def test_read_army(self):
+        response = self.client.get(url_for('home'))
+        self.assertIn(b"Figure 1", response.data)
+        self.assertIn(b"8", response.data)
+        self.assertIn(b"Space Marine", response.data)
+        self.assertIn(b"1", response.data)
+        
+
+class TestCreate1(TestBase):
+    def test_create_army(self):
+        response = self.client.post(url_for('add_figure'),
+        data=dict(name='Figure 2', number_of_models=9, faction='Space Marines', army_id=1),
+        follow_redirects=True
+        )
+        self.assertIn(b"Figure 2", response.data)
+        self.assertIn(b"9", response.data)
+        self.assertIn(b"Space Marine", response.data)
+        self.assertIn(b"1", response.data)
+
+class TestUpdate1(TestBase):
+    def test_update_army(self):
+        response = self.client.post(url_for('update_figure', id=1),
+        data=dict(name="Update figure", number_of_models=9, faction='Space Marines', army_id=1),
+        follow_redirects=True
+        )
+        self.assertIn(b"Update figure", response.data)
+        self.assertIn(b"9", response.data)
+        self.assertIn(b"Space Marine", response.data)
+        self.assertIn(b"1", response.data)
+
+class TestDelete1(TestBase):
+    def test_delete_army(self):
+        response = self.client.get(url_for('delete_figure', id=1),
+        follow_redirects=True
+        )
+        self.assertNotIn(b"Figure 1", response.data)
+        self.assertNotIn(b"8", response.data)
+        self.assertNotIn(b"Space Marine", response.data)
